0245ec1097ab2078bebc97fc1373381835526ea8
iamchoking <iamchoking247@gmail.com>
2020-09-08 07:40:00 -0700

    repository expansion (to all ee209)

---
+++ 0-ee209-preliminary/precept/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world!\nI only have a slight Idea of the time that I used to know what I am doing\n^_^ hope you dont fuck this one up boi ee209\n");
+    return 0;
+}
